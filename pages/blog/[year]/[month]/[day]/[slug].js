3980aa47cfa25b947d9fa352889e93f6b8ab6d7e
Yukai Huang <yukaihuangtw@gmail.com>
2020-10-06 08:46:08 +0800

    Display title

--- pages/blog/[year]/[month]/[day]/[slug].js
+++ pages/blog/[year]/[month]/[day]/[slug].js
@@ -1,9 +1,15 @@
+import Head from 'next/head'
+
 import { getAllPostsWithSlug, formatPostsAsParams, getPostData } from '../../../../../lib/post'
 import { render } from '../../../../../lib/markdown'
 import Header from '../../../../../components/Header'
 
 export default function Post({ content, title }) {
   return <div className='container'>
+    <Head>
+      <title>{ title } | Daily Oops!</title>
+    </Head>
+
     <Header />
     <div className='markdown-body mx-auto pt-4 pb-6' dangerouslySetInnerHTML={{ __html: render(content) }} style={{ maxWidth: 700 }} />
   </div>
