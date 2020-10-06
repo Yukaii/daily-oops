001c7a123f7cdc7ee95c878ada142e8ebad43fa5
Yukai Huang <yukaihuangtw@gmail.com>
2020-10-06 08:40:27 +0800

    Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
 import { getAllPostsWithSlug } from '../lib/post'
+import Header from '../components/Header'
 
 export default function Home({ posts }) {
+
+  const postRows = posts.map(post => {
+  const { date: { year, month, day }, slug } = post
+
+
+  return <div className="Box-row d-flex flex-items-center" key={post.id}>
+    <div className="flex-auto">
+      <Link href={`/blog/${year}/${month}/${day}/${slug}`}>
+        <a>
+          <strong>{post.title}</strong>
+        </a>
+      </Link>
+
+      <div className="text-small text-gray-light">
+        Description
+      </div>
+    </div>
+  </div>
+  })
+
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Daily Oops!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container markdown-body">
-        <ul>
-          { posts.map(post => {
-            const { date: { year, month, day }, slug } = post
+      <Header />
 
-            return <li key={post.id}>
-              <a href={`/blog/${year}/${month}/${day}/${slug}`}>{post.title}</a>
-            </li>}
-            ) }
-        </ul>
+      <div className="d-block mx-auto markdown-body py-4" style={{ maxWidth: 700 }}>
+        <div className="Box">
+          {postRows}
+        </div>
       </div>
     </div>
   )
