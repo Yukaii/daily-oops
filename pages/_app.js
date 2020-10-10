2f0e09c32a2ccbb856d547ec38f7298ed9854059
Yukai Huang <yukaihuangtw@gmail.com>
2020-10-10 17:18:28 +0800

    User scalable no

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import Head from 'next/head'
+
 import '@primer/css/index.scss'
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
