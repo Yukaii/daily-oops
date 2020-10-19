61b6cc04f7483f18797825325ec11d49eaa0f9dc
Yukai Huang <yukaihuangtw@gmail.com>
2020-10-19 22:28:21 +0800

    Css column projects layout

--- pages/projects.js
+++ pages/projects.js
@@ -12,16 +12,19 @@ export default function Projects ({ projects }) {
 
     <Header />
 
-    <div className='markdown-body mx-auto pt-4 pb-6 px-3 container-lg' style={{ maxWidth: 700 }}>
+    <div className='markdown-body mx-auto pt-4 pb-6 px-3 container-lg' style={{ maxWidth: 700, columns: '6 250px', columnGap: '1rem' }}>
       { projects.map(project => {
-        return <div className='col-12 col-sm-6 d-inline-block'>
-        <a className="d-inline-block box-shadow-medium mx-2 px-3 pt-4 pb-6 position-relative rounded-1 overflow-hidden no-underline" href={project.link} target="_blank" style={{ width: 'calc(100% - 16px)', height: 200 }}>
-          <h3 className="text-gray-dark">{ project.title }</h3>
-          <p className="text-gray">
-            { project.description }
-          </p>
+        return <a className="Box d-inline-block ma-2 mb-5 overflow-hidden no-underline" href={project.link} target="_blank" style={{ width: 'calc(100% - 16px)', height: '100%' }}>
+          {
+            project.image && <img src={project.image} style={{ borderRadius: 0 }} />
+          }
+          <div className='px-3 pa-2'>
+            <h3 className="text-gray-dark">{ project.title }</h3>
+            <p className="text-gray">
+              { project.description }
+            </p>
+          </div>
         </a>
-        </div>
       }) }
     </div>
   </div>
