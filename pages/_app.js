5323fde183692a7a6a17269e10a26d694d3c241a
Yukai Huang <yukaihuangtw@gmail.com>
2021-04-05 18:43:54 +0800

    Change html attribute on fly

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import SimpleReactLightbox from 'simple-react-lightbox'
 import '@fortawesome/fontawesome-free/js/all'
 
 import { pageview } from 'lib/gtag'
+import { usePrimerDarkMode } from 'lib/usePrimerDarkMode'
 
 import '@primer/css/index.scss'
 import 'styles/globals.scss'
@@ -14,6 +15,9 @@ import 'highlight.js/styles/github-gist.css'
 Router.events.on('routeChangeComplete', (url) => process.env.NODE_ENV === 'production' && pageview(url))
 
 function MyApp({ Component, pageProps }) {
+  
+  usePrimerDarkMode()
+
   return <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
