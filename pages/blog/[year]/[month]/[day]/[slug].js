8843233a72366b05f97fcf8205ccee9d664d8221
Yukai Huang <yukaihuangtw@gmail.com>
2021-04-05 18:05:43 +0800

    Bump pacakge versions

--- pages/blog/[year]/[month]/[day]/[slug].js
+++ pages/blog/[year]/[month]/[day]/[slug].js
@@ -22,7 +22,12 @@ export default function Post({ content, title, params, disqus }) {
     <div className='container pt-4 pb-3 px-3'>
       <span className='text-mono text-gray-light'>{ date.format('LL') }</span>
     </div>
-    <SRLWrapper>
+    <SRLWrapper options={{
+      settings: {
+        lightboxTransitionSpeed: 0.3,
+        slideTransitionSpeed: 0.3
+      }
+    }}>
       <Markdown content={content} className='container pb-6 px-3' />
     </SRLWrapper>
     {
