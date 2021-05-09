18b3985ab70d0adfd8fed53e260d1a421e7b8f3a
Yukai Huang <yukaihuangtw@gmail.com>
2021-05-09 22:51:26 +0800

    Add north start icon to postrow

--- components/PostRow.js
+++ components/PostRow.js
@@ -1,11 +1,15 @@
 import dayjs from 'lib/dayjs'
 import Link from 'next/link'
+import { NorthStarIcon } from '@primer/octicons-react'
 
 export default function PostRow ({ post, index, totalCount }) {
   const { date: { year, month, day }, slug } = post
   const date = dayjs(`${year}-${month}-${day}`)
 
-  return <div className="Box-row Box-row--hover-gray d-flex flex-items-center">
+  return <div className="Box-row Box-row--hover-gray d-flex flex-items-start">
+    <div className='mr-2 mt-1 d-flex flex-items-start' style={{ color: 'var(--color-scale-red-2 )' }}>
+      <NorthStarIcon />
+    </div>
     <div className="flex-auto">
       <Link href={`/blog/${year}/${month}/${day}/${slug}`}>
         <a>
