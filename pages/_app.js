942c454f7698c1b5b91f5acf675779108a820aa2
Yukai Huang <yukaihuangtw@gmail.com>
2021-05-14 00:56:00 +0800

    Remove user-scalable

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
 
   return <>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       <meta name="color-scheme" content="dark light" />
     </Head>
     <SimpleReactLightbox>
