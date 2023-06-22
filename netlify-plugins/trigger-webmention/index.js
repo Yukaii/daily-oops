ae08caa55f6308bbbfbccddfeb234b0e10cbcf91
Yukai Huang <yukaihuangtw@gmail.com>
2023-06-22 20:34:36 +0800

    fix: add es module

--- netlify-plugins/trigger-webmention/index.js
+++ netlify-plugins/trigger-webmention/index.js
@@ -1,6 +1,8 @@
+import fs from 'fs'
+
 export const onEnd = async function () {
   console.log(process.cwd())
   // list all files in the current directory
-  const files = await fs.readdir(process.cwd())
+  const files = fs.readdirSync(process.cwd())
   console.log(files)
 }
