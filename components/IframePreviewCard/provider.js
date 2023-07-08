6ab8adc040b92e1e327340c9dbf2e2de6671e3b0
Yukai Huang <yukaihuangtw@gmail.com>
2023-07-08 22:28:31 +0800

    feat: holding shift
    yeah, arc inspired

--- components/IframePreviewCard/provider.js
+++ components/IframePreviewCard/provider.js
@@ -4,6 +4,45 @@ import React, { useEffect, useMemo, useState } from 'react'
 
 import IframePreviewCard from './card'
 
+function isExternalLink(url) {
+  // compare with current location
+  try {
+    const urlObj = new URL(url)
+
+    return urlObj.origin !== window.location.origin
+  } catch (e) {
+    return false
+  }
+}
+
+const useHoldingShiftKey = () => {
+  const [, setIsHoldingShift, isHoldingShiftRef] = useStateRef(false)
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'Shift') {
+        setIsHoldingShift(true)
+      }
+    }
+
+    const handleKeyUp = (e) => {
+      if (e.key === 'Shift') {
+        setIsHoldingShift(false)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener('keyup', handleKeyUp)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+      window.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [setIsHoldingShift])
+
+  return isHoldingShiftRef
+}
+
 // Note: This is a provider for the IframePreviewCard component
 // It should register the clicking event of any external link
 // and bring up the IframePreviewCard component
@@ -16,27 +55,34 @@ export const IframePreviewCardProvider = ({ children }) => {
     width > 768
   )
 
+  const isHoldingAltRef = useHoldingShiftKey()
+
   useMemo(() => {
     setPreviewCardEnabled(width > 768)
   }, [setPreviewCardEnabled, width])
 
   useEffect(() => {
     const handlePreview = (e) => {
-      if (!previewCardEnabledRef.current) {
+      const { target } = e
+      const url = target.href
+
+      if (
+        !isHoldingAltRef.current ||
+        !previewCardEnabledRef.current ||
+        !isExternalLink(url)
+      ) {
         return
       }
 
-      const { target } = e
-      if (target.tagName === 'A' && target.href) {
+      if (target.tagName === 'A' && url) {
         e.preventDefault()
         setPreviewUrl(target.href)
         setShowPreview(true)
       }
     }
-
     document.addEventListener('click', handlePreview)
     return () => document.removeEventListener('click', handlePreview)
-  }, [])
+  }, [isHoldingAltRef, previewCardEnabledRef])
 
   return (
     <>
