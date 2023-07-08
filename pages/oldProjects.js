2925cb10394591b1a16d5101fd49865ccc4bfe29
Yukai Huang <yukaihuangtw@gmail.com>
2023-07-08 13:32:32 +0800

    New projects page (#42)

---
+++ pages/oldProjects.js
@@ -0,0 +1,46 @@
+import { loadProjects } from 'lib/project'
+import Head from 'next/head'
+
+export default function Projects({ projects }) {
+  return (
+    <div>
+      <Head>
+        <title>Projects | Daily Oops!</title>
+      </Head>
+
+      <div
+        className="markdown-body mx-auto pt-4 pb-6 px-3 container-lg"
+        style={{ maxWidth: 700, columns: '6 250px', columnGap: '1rem' }}
+      >
+        {projects.map((project) => {
+          return (
+            <a
+              className="Box d-inline-block ma-2 mb-5 overflow-hidden no-underline"
+              href={project.link}
+              target="_blank"
+              style={{ width: 'calc(100% - 16px)', height: '100%' }}
+              rel="noreferrer"
+              key={project.link}
+            >
+              {project.image && (
+                <img src={project.image} style={{ borderRadius: 0 }} />
+              )}
+              <div className="px-3 pa-2">
+                <h3 className="color-text-primary">{project.title}</h3>
+                <p className="color-text-secondary">{project.description}</p>
+              </div>
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      projects: loadProjects(),
+    },
+  }
+}
--- pages/projects.js
+++ pages/projects.js
@@ -1,46 +1,33 @@
-import { loadProjects } from 'lib/project'
+import Markdown from 'components/Markdown'
+import { loadProjectMarkdown } from 'lib/project'
 import Head from 'next/head'
 
-export default function Projects({ projects }) {
+export default function Projects({ content }) {
   return (
     <div>
       <Head>
         <title>Projects | Daily Oops!</title>
       </Head>
 
-      <div
-        className="markdown-body mx-auto pt-4 pb-6 px-3 container-lg"
-        style={{ maxWidth: 700, columns: '6 250px', columnGap: '1rem' }}
-      >
-        {projects.map((project) => {
-          return (
-            <a
-              className="Box d-inline-block ma-2 mb-5 overflow-hidden no-underline"
-              href={project.link}
-              target="_blank"
-              style={{ width: 'calc(100% - 16px)', height: '100%' }}
-              rel="noreferrer"
-              key={project.link}
-            >
-              {project.image && (
-                <img src={project.image} style={{ borderRadius: 0 }} />
-              )}
-              <div className="px-3 pa-2">
-                <h3 className="color-text-primary">{project.title}</h3>
-                <p className="color-text-secondary">{project.description}</p>
-              </div>
-            </a>
-          )
-        })}
-      </div>
+      <Markdown
+        content={`# Projects
+Here are some of my projects that I've worked on.
+
+For older peojcts, please visit my [GitHub](https://github.com/Yukaii), or take a look at [here](./oldProjects).
+`}
+        className="container pt-4 pb-2 px-3"
+      />
+
+      <Markdown content={content} className="container pt-4 pb-6 px-3" />
     </div>
   )
 }
 
 export async function getStaticProps() {
+  const content = await loadProjectMarkdown()
   return {
     props: {
-      projects: loadProjects(),
+      content,
     },
   }
 }
