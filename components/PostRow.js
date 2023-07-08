73c1066ba7ed4f47e38e21e9984231b73b88d807
Yukai Huang <yukaihuangtw@gmail.com>
2023-07-08 16:18:28 +0800

    feat: read status

--- components/PostRow.js
+++ components/PostRow.js
@@ -2,6 +2,8 @@ import { DotFillIcon } from '@primer/octicons-react'
 import dayjs from 'lib/dayjs'
 import Link from 'next/link'
 
+import useReadStatus from '../lib/hooks/useReadStatus'
+
 export default function PostRow({ post, index, totalCount }) {
   const {
     date: { year, month, day },
@@ -9,16 +11,31 @@ export default function PostRow({ post, index, totalCount }) {
   } = post
   const date = dayjs(`${year}-${month}-${day}`)
 
+  const fullSlug = `/blog/${year}/${month}/${day}/${slug}`
+
+  const [readStatus, setReadStatus] = useReadStatus(fullSlug)
+
+  const onLinkClick = () => {
+    setReadStatus(true)
+  }
+
   return (
     <div className="Box-row Box-row--hover-gray d-flex flex-items-start">
       <div
         className="mr-2 mt-1 d-flex flex-items-start"
         style={{ color: 'var(--color-scale-orange-3)' }}
       >
-        <DotFillIcon />
+        {readStatus ? (
+          <div style={{ width: 16, height: 16 }} />
+        ) : (
+          <DotFillIcon />
+        )}
       </div>
       <div className="flex-auto">
-        <Link href={`/blog/${year}/${month}/${day}/${slug}`}>
+        <Link
+          href={`/blog/${year}/${month}/${day}/${slug}`}
+          onClick={onLinkClick}
+        >
           <strong>{post.title}</strong>
         </Link>
 
